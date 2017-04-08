c6ed073d8401585b7bffde871989b97a38950d5a
kurosawa <kuro6412@gmail.com>
2017-04-08 22:24:38 +0900

    add picture page and user page

---
+++ db/migrate/20170408113759_create_pictures.rb
@@ -0,0 +1,11 @@
+class CreatePictures < ActiveRecord::Migration[5.0]
+  def change
+    create_table :pictures do |t|
+      t.string :title
+      t.string :image
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
